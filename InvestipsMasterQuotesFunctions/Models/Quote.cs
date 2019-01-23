4547774fe9f7c7a54439968c1ec460e02f048b3f
Carlos Calderon <carloscal@omnicell.com>
2019-01-22 19:06:08 -0600

    add initial test function and initial Model

---
+++ InvestipsMasterQuotesFunctions/Models/Quote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestipsMasterQuotesFunctions.Models
+{
+    [Table("Quote", Schema = "Stock")]
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal Open { get; set; }
+    }
+}
